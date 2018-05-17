dd6aded4dba22548012a93eee15ecb17f17593a4
Sabrina Gill <39250291+sabrupu@users.noreply.github.com>
2018-05-17 13:59:25 -0400

    Add files via upload

---
+++ Practice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int userValue = 2;
+            //string message = (userValue == 1) ? "boat" : "car";
+            //Console.WriteLine("{1} - {0}", userValue, message);
+
+         /*   if (userValue == "BOb")
+                myValue = "You typed 'Bob'";
+            myValue = "You typed 'Steve'";
+            else if (userValue == "Brian")
+                myValue = "You typed 'Brian'";
+
+            Console.ReadLine(); */
+
+
+        }
+    }
+}
